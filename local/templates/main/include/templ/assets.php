c9996f448b31a8fb50625fd9e509c49016a786b8
Nikita <nikita.ableev@mail.ru>
2024-02-20 13:37:36 +0300

    add dir include

---
+++ local/templates/main/include/templ/assets.php
@@ -0,0 +1,11 @@
+<?php
+
+use Bitrix\Main\Page\Asset;
+
+$asset = Asset::getInstance();
+
+$asset->addCss(SITE_TEMPLATE_PATH . '/590.css');
+$asset->addCss(SITE_TEMPLATE_PATH . '/app.css');
+
+$asset->addJs(SITE_TEMPLATE_PATH . '/assets/app.js');
+$asset->addJs(SITE_TEMPLATE_PATH . '/assets/vendor.js');
